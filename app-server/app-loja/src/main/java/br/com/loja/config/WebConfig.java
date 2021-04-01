649043e187c847808c04722b37a8d133c041f99a
Leandro Reis <developer.reis@gmail.com>
2021-03-31 21:57:17 -0300

    commit finalizado

--- app-server/app-loja/src/main/java/br/com/loja/config/WebConfig.java
+++ app-server/app-loja/src/main/java/br/com/loja/config/WebConfig.java
@@ -7,8 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
+		registry.addMapping("/**")
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
 	}
-	
 }
